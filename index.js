46ba45f0fb4f2e101d3b890fcb33ed082e0e8290
hellen kimani <astridlee80@gmail.com>
2024-04-04 10:05:36 +1000

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+Math.floor(Math.random() * 10) + 1;
+Math.max(1, 2, 3, 4, 5);
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100); // Generates a random integer greater than 0
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
